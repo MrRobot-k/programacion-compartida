ab4177bb1a9fb486926a30d65dc5966a936f2180
MrRobot-k <esdraswarzone@gmail.com>
2025-11-14 17:39:59 +0000

    Vercel de json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   },
+  server: {
+    host: true
+  }
 })
